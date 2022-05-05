2f686122c4463122632043ebca4b3e195185f4b4
lyphrowny <strukov.as@edu.spbstu.ru>
2022-05-05 21:18:57 +0300

    added lab3; moved the tables into subdirs

---
+++ lab1_4/lab3.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lab2 import _z_p
+from setup import set_up
+
+
+def _q1(xs):
+    return _z_p(xs, 0.25)
+
+
+def _q3(xs):
+    return _z_p(xs, 0.75)
+
+
+def _boxplot(distr, ps_num, dir):
+    title = distr.__class__.__name__
+    plt.title(title)
+    data = list(map(distr.get_rvs, ps_num))
+    plt.boxplot(data, labels=ps_num, vert=False)
+    plt.ylabel("n")
+
+    if not dir.exists():
+        dir.mkdir()
+    plt.savefig(dir.joinpath(title))
+    plt.close()
+
+
+def _outlier(distr, ps_num, times=1000):
+    r = []
+    for p_num in ps_num:
+        cnt = 0
+        for _ in range(times):
+            x = np.sort(distr.get_rvs(p_num))
+            q1, q3 = _q1(x), _q3(x)
+            lo, hi = q1 - 1.5 * (q3 - q1), q3 + 1.5 * (q3 - q1)
+            cnt += len(x[np.logical_or(x < lo, hi < x)])
+        r.append(f"{distr.__class__.__name__}, n={p_num} & {cnt / (times * p_num)} \\\\\n")
+    return r
+
+
+def lab3(distrs, ps_num, plot_dir, table_dir, times=1000):
+    table = []
+    for distr in distrs:
+        _boxplot(distr, ps_num, plot_dir)
+        table.extend(_outlier(distr, ps_num, times))
+    if not table_dir.exists():
+        table_dir.mkdir(parents=True)
+    dest = table_dir.joinpath("outliers")
+    dest.write_text("".join(table))
+
+
+if __name__ == "__main__":
+    distrs = set_up()
+    lab3(distrs, [20, 100], Path(""), Path(""))
--- lab1_4/main.py
+++ lab1_4/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 from setup import set_up
 
 
@@ -11,7 +12,8 @@ def main():
     table_dir = Path("lab1_4/tables")
 
     lab1(distrs, [10, 50, 1000], path=img_dir.joinpath("lab1"))
-    lab2(distrs, [10, 100, 1000], dir=table_dir)
+    lab2(distrs, [10, 100, 1000], dir=table_dir.joinpath("lab2"))
+    lab3(distrs, [20, 100], plot_dir=img_dir.joinpath("lab3"), table_dir=table_dir.joinpath("lab3"))
 
 
 if __name__ == "__main__":
