5c81bc092de3bfca49d668c939cd4df57b27e9c4
lyphrowny <strukov.as@edu.spbstu.ru>
2022-05-05 16:50:39 +0300

    added lab2 task and table generation routine

---
+++ lab1_4/lab2.py
@@ -0,0 +1,85 @@
+from collections import defaultdict
+from itertools import product
+from pathlib import Path
+
+import numpy as np
+
+from setup import set_up
+
+
+def _mean(xs):
+    return sum(xs) / len(xs)
+
+
+def _median(xs):
+    # -1 since indexing starts with 0
+    n = len(xs) // 2 - 1
+    # the oddity condition is reversed due to `-1` above
+    return [xs[n + 1], sum(xs[n:n + 2]) / 2][n & 1]
+
+
+def _z_r(xs):
+    return (xs[0] + xs[~0]) / 2
+
+
+def _z_p(xs, p):
+    # -1 due to indexing start from 0
+    # dot besides 1 is to convert `np` to float, as this type
+    # has `.is_integer()` method
+    np = len(xs) * p - 1.
+    return xs[int(np) + (not np.is_integer())]
+
+
+def _z_q(xs, ps):
+    return sum(_z_p(xs, p) for p in ps) / len(ps)
+
+
+def _tr_med(xs, r=0.25):
+    n = len(xs)
+    nr = n * r
+    return 1 / (n - 2 * nr) * sum(xs[int(nr) + 1:n - int(r) + 1])
+
+
+def _make_table(distr, ps_num, char_names, chars):
+    n = len(char_names)
+    tol = 6
+    pre = f"{distr.__class__.__name__} n={ps_num}{' & ' * n}\\\\\n" \
+          f"& {' & '.join(f'${name}$' for name in char_names)} \\\\\n"
+    mid = ''.join(f"{name} & {' & '.join(f'{ch:.{tol}f}' for ch in char)} \\\\\n" for name, char in chars.items())
+    suf = f"\\hline \\\\\n"
+    return pre + mid + suf
+
+
+def lab2(distrs, ps_num, dir, times=1000):
+    for distr in distrs:
+        if not dir.exists():
+            dir.mkdir(parents=True)
+        dest = dir.joinpath(distr.__class__.__name__)
+        dest.write_text(_gen_table(distr, ps_num, times))
+
+
+def _gen_table(distr, ps_num, times=1000):
+    d = defaultdict(list)
+    table = []
+    for p_num in ps_num:
+        for _ in range(times):
+            x = sorted(distr.get_rvs(p_num))
+            d[r"\bar{x}"].append(_mean(x))
+            d["med x"].append(_median(x))
+            d["z_R"].append(_z_r(x))
+            d["z_Q"].append(_z_q(x, [0.25, 0.75]))
+            d["z_tr"].append(_tr_med(x))
+        chars = defaultdict(list)
+        for v in d.values():
+            chars["E(z)"].append(_mean(v))
+            chars["D(z)"].append(_mean(np.power(v, 2)) - np.power(chars["E(z)"][~0], 2))
+            chars["E(z) - \\sqrt(D(z))"].append(chars["E(z)"][~0] - np.sqrt(chars["D(z)"][~0]))
+            chars["E(z) + \\sqrt(D(z))"].append(chars["E(z)"][~0] + np.sqrt(chars["D(z)"][~0]))
+        table.append(_make_table(distr, p_num, d.keys(), chars))
+    return "".join(table)
+
+
+if __name__ == "__main__":
+    print(_z_q([1, 2, 3, 4], [0.25, 0.75]))
+    distrs = set_up()
+    lab2(distrs, [10, 100, 1000], Path("./data/tables"))
--- lab1_4/main.py
+++ lab1_4/main.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
 from lab1 import lab1
+from lab2 import lab2
 from setup import set_up
 
 
 def main():
     distrs = set_up()
     img_dir = Path("lab1_4/imgs")
+    table_dir = Path("lab1_4/tables")
 
     lab1(distrs, [10, 50, 1000], path=img_dir.joinpath("lab1"))
+    lab2(distrs, [10, 100, 1000], dir=table_dir)
 
 
 if __name__ == "__main__":
